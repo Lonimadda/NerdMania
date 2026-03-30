9e283ae3b495f1dd3a960381fcc2f4a39f287e28
stefano7703 <162974312+stefano7703@users.noreply.github.com>
2026-03-30 15:05:52 +0200

    modify: User.java

--- NerdMania/src/main/java/it/Gruppo/NerdMania/Modelli/User.java
+++ NerdMania/src/main/java/it/Gruppo/NerdMania/Modelli/User.java
@@ -16,4 +16,23 @@ import java.util.ArrayList;
 @NoArgsConstructor
 @Table(name = "user", schema = "NerdMania")
 public class User {
+
+
+    @Id
+    private String username;
+
+    private String nome;
+    private String cognome;
+    private String email;
+    private String password;
+    private Boolean cartaFedelta;
+
+    @OneToOne(mappedBy = "user")
+    private Carrello carrello;
+
+    @OneToMany(mappedBy = "user",  cascade = CascadeType.ALL)
+    private List<Ordine> ordini;
+
+
+
 }
