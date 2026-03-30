373335d15ef1e55f003392f047694e75e8c0f09c
Mattia Maddaloni <mattiamaddalo2004@gmail.com>
2026-03-30 14:52:11 +0200

    .gitignore jarRepositories.xml misc.xml NerdMania.iml Prodotto.java

---
+++ NerdMania/src/main/java/it/Gruppo/NerdMania/Modelli/Prodotto.java
@@ -0,0 +1,34 @@
+package it.Gruppo.NerdMania.Modelli;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "Prodotto", schema = "NerdMania")
+public class Prodotto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private String nome;
+
+    private long id;
+
+    private Double prezzo;
+
+    private float peso;
+
+    private String descrizione;
+
+    @ManyToMany(mappedBy = "prodotti")
+    private List<Ordine> ordini;
+}
