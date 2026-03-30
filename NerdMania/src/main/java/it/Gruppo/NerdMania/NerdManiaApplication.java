d41de2990b7008c937e8bb3d126e35a91e1f88c6
Lonimadda <mattiamaddalo2004@gmail.com>
2026-03-30 12:54:14 +0200

    starter file

---
+++ NerdMania/src/main/java/it/Gruppo/NerdMania/NerdManiaApplication.java
@@ -0,0 +1,13 @@
+package it.Gruppo.NerdMania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerdManiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NerdManiaApplication.class, args);
+	}
+
+}
---
+++ NerdMania/src/test/java/it/Gruppo/NerdMania/NerdManiaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.Gruppo.NerdMania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NerdManiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
