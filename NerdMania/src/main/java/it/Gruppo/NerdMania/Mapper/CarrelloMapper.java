dff3b9f1c821c31d8a0853cb3a7b8c70ecd6aafa
Mor33mila <lorenzolomb20@gmail.com>
2026-03-30 16:44:40 +0200

    Add. CarrelloMapper.java      CatalogoMapper.java

---
+++ NerdMania/src/main/java/it/Gruppo/NerdMania/Mapper/CarrelloMapper.java
@@ -0,0 +1,21 @@
+package it.Gruppo.NerdMania.Mapper;
+
+import it.Gruppo.NerdMania.DTO.CarrelloDto;
+import it.Gruppo.NerdMania.Modelli.Carrello;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CarrelloMapper extends AbstractConverter<Carrello, CarrelloDto> {
+
+
+    //modelmapper usato per mappare automaticamente
+    final private ModelMapper mapper = new ModelMapper();
+
+    // converte entità Auto in DTO AutoDto
+    @Override
+    public CarrelloDto toDTO(Carrello entity) { return mapper.map(entity, CarrelloDto.class); }
+
+    //converte DTO AutoDto in entità Auto
+    @Override
+    public Carrello toEntity(CarrelloDto dto) { return mapper.map(dto, Carrello.class);} }
