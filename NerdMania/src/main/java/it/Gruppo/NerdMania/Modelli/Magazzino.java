bc42f8a20a5e9aada4711d1a09d591b7dbdbfa69
Mattia Maddaloni <mattiamaddalo2004@gmail.com>
2026-03-30 15:45:54 +0200

    Magazzino.java MagazzinoDto.java Prodotto.java

---
+++ NerdMania/src/main/java/it/Gruppo/NerdMania/Modelli/Magazzino.java
@@ -0,0 +1,37 @@
+package it.Gruppo.NerdMania.Modelli;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "Magazzino", schema = "NerdMania")
+public class Magazzino {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String indirizzo;
+
+    private String codice;
+
+    private int quantita;
+
+
+    @ManyToMany
+    @JoinTable(
+            name = "magazzino_prodotto",
+            schema = "NerdMania",
+            joinColumns = @JoinColumn(name = "magazzino_id"),
+            inverseJoinColumns = @JoinColumn(name = "prodotto_id")
+    )
+    private List<Prodotto> prodotti;
+}
