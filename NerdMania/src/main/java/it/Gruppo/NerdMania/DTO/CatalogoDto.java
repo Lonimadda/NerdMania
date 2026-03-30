b847f7ed96bf0822a9951bf81e79720ea98a6ee4
stefano7703 <162974312+stefano7703@users.noreply.github.com>
2026-03-30 15:58:47 +0200

    modify: Catalogo.java         CatalogoDto.java

--- NerdMania/src/main/java/it/Gruppo/NerdMania/DTO/CatalogoDto.java
+++ NerdMania/src/main/java/it/Gruppo/NerdMania/DTO/CatalogoDto.java
@@ -1,21 +1,19 @@
 package it.Gruppo.NerdMania.DTO;
 
-import it.Gruppo.NerdMania.Modelli.Prodotto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
-
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
-public class CategoriaDto {
+@NoArgsConstructor
+public class CatalogoDto {                              //LorenzoLombardi
 
     private Long id;
+
     private String nome;
-    private Catalogo catalogo;
-    private List<Prodotto> prodotti;
+
+    private List<Categoria> categorie;
 }
