c10c124b617df4a98340d6def0079c70adc740f3
adinoolfi <giuseppe.adinolfi.01@gmail.com>
2026-03-31 16:41:01 +0200

    add:  AbstractController.java OrdineRepository.java
    modify: OrdineController.java
            OrdineService.java
            pom.xml
            UserService.java
            OrdineDto.java

---
+++ NerdMania/src/main/java/it/Gruppo/NerdMania/Controller/SpedizioneController.java
@@ -0,0 +1,56 @@
+package it.Gruppo.NerdMania.Controller;
+
+import it.Gruppo.NerdMania.DTO.OrdineDto;
+import it.Gruppo.NerdMania.DTO.SpedizioneDto;
+import it.Gruppo.NerdMania.Service.OrdineService;
+import it.Gruppo.NerdMania.Service.SpedizioneService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("Spedizione")
+@CrossOrigin(origins = "http://localhost:8080")
+public class SpedizioneController extends AbstractController<SpedizioneDto> {
+
+    @Autowired
+    private SpedizioneService service;
+
+    @GetMapping("/findByFragileTrue")
+    public List<SpedizioneDto> findByFragileTrue() {
+        return service.findByFragileTrue();
+    }
+
+    @GetMapping("/findByEsteroTrue")
+    public List<SpedizioneDto> findByEsteroTrue() {
+        return service.findByEsteroTrue();
+    }
+
+    @GetMapping("/findByPesoGreaterThan")
+    public List<SpedizioneDto> findByPesoGreaterThan(@RequestParam("peso") float peso) {
+        return service.findByPesoGreaterThan(peso);
+    }
+
+    @GetMapping("/findByPesoLessThan")
+    public List<SpedizioneDto> findByPesoLessThan(@RequestParam("peso") float peso) {
+        return service.findByPesoLessThan(peso);
+    }
+
+    @GetMapping("/findByAltezzaBetween")
+    public List<SpedizioneDto> findByAltezzaBetween(@RequestParam float min,
+                                                    @RequestParam float max) {
+        return service.findByAltezzaBetween(min,max);
+    }
+
+    @GetMapping("/findByOrdineId")
+    public List<SpedizioneDto> findByOrdineId(Integer id) {
+        return service.findByOrdineId(id);
+    }
+
+    @GetMapping("/findByAltezzaGreaterThanOrLunghezzaGreaterThan")
+    public List<SpedizioneDto> findByAltezzaGreaterThanOrLunghezzaGreaterThan(@RequestParam float h,
+                                                                              @RequestParam float l) {
+        return service.findByAltezzaGreaterThanOrLunghezzaGreaterThan(h,l);
+    }
+}
