0f7d5600cc8d870ad44e5974f922a418a07b33e9
stefano7703 <162974312+stefano7703@users.noreply.github.com>
2026-03-31 09:59:04 +0200

    add: AbstractService.java      ServiceDTO.java      UserRepository.java      UserService.java modify: application.properties

---
+++ NerdMania/src/main/java/it/Gruppo/NerdMania/Service/AbstractService.java
@@ -0,0 +1,40 @@
+package it.Gruppo.NerdMania.Service;
+
+import it.Gruppo.NerdMania.Mapper.Converter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public abstract class AbstractService<ENTITY, DTO> implements ServiceDTO<DTO> {
+
+    protected JpaRepository<ENTITY, Integer> repository;
+    protected Converter<ENTITY, DTO> converter;
+
+    public AbstractService(JpaRepository<ENTITY, Integer> repository, Converter<ENTITY, DTO> converter) {
+        this.repository = repository;
+        this.converter = converter;
+    }
+
+    @Override
+    public DTO insert (DTO dto){
+        return converter.toDTO(repository.save(converter.toEntity(dto)));
+    }
+
+    @Override
+    public DTO update (DTO dto){
+        return converter.toDTO(repository.save(converter.toEntity(dto)));
+    }
+
+    @Override
+    public Iterable<DTO> getAll(){
+        return converter.toDTOList(repository.findAll());
+    }
+
+    @Override
+    public DTO read (Integer id){
+        return converter.toDTO(repository.findById(id).get());
+    }
+
+    @Override
+    public void delete(Integer id) {
+        repository.deleteById(id);
+    }
+}
