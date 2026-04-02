3b0280b7d92ace1f407c35122be13ed790cabfe9
adinoolfi <giuseppe.adinolfi.01@gmail.com>
2026-04-02 12:11:30 +0200

    profili

--- NerdMania/src/main/java/it/Gruppo/NerdMania/Configuration/SecurityConfig.java
+++ NerdMania/src/main/java/it/Gruppo/NerdMania/Configuration/SecurityConfig.java
@@ -3,6 +3,7 @@ package it.Gruppo.NerdMania.Configuration;
 import it.Gruppo.NerdMania.Security.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,6 +20,7 @@ import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
+@Profile("secure")
 public class SecurityConfig {
 
     private final CustomUserDetailsService  userDetailsService;
