a1e952a09bb067cb24b88349411845f0e42cd19c
adinoolfi <giuseppe.adinolfi.01@gmail.com>
2026-04-03 16:59:28 +0200

    DocumentazioneNerdmania.docx

--- NerdMania/src/main/java/it/Gruppo/NerdMania/Repository/OrdineRepository.java
+++ NerdMania/src/main/java/it/Gruppo/NerdMania/Repository/OrdineRepository.java
@@ -9,11 +9,24 @@ import java.util.List;
 @Repository
 public interface OrdineRepository extends JpaRepository<Ordine, Integer> {
 
+    //METODO CHE CERCA GLI ORDINI IN BASE ALL'USERNAME DELL'UTENTE
     List<Ordine> findByUserUsername(String username);
+
+    //METODO CHE CERCA GLI ORDINI IN ORDINE DECRESCENTE PER COSTO TOTALE
     List<Ordine> findAllByOrderByCostoTotaleDesc();
+
+    //METODO CHE CERCA GLI ORDINI IN ORDINE CRESCENTE PER COSTO TOTALE
     List<Ordine> findAllByOrderByCostoTotaleAsc();
+
+    //METODO CHE CERCA GLI ORDINI IN BASE ALL'INDIRIZZO DI SPEDIZIONE
     List<Ordine> findByIndirizzoSpedizioneContainingIgnoreCase(String testo);
+
+    //METODO CHE CERCA GLI ORDINI IN BASE AL COSTO TOTALE MAGGIORE DI UN CERTO INPUT
     List<Ordine> findByCostoTotaleGreaterThan(float prezzo);
+
+    //METODO CHE CERCA GLI ORDINI IN BASE AL COSTO TOTALE MINORE DI UN CERTO INPUT
     List<Ordine> findByCostoTotaleLessThan(float prezzo);
+
+    //METODO CHE CERCA GLI ORDINI CHE CONTENGONO UN CERTO PRODOTTO
     List<Ordine> findByProdottiId(Integer prodottoId);
 }
