186e4d68f2d31bc675566eec2c0e029a9589bedd
adinoolfi <giuseppe.adinolfi.01@gmail.com>
2026-04-23 17:21:40 +0200

    modifiche

--- NerdMania/src/main/java/it/Gruppo/NerdMania/Service/OrdineService.java
+++ NerdMania/src/main/java/it/Gruppo/NerdMania/Service/OrdineService.java
@@ -3,8 +3,8 @@ package it.Gruppo.NerdMania.Service;
 import it.Gruppo.NerdMania.DTO.OrdineDto;
 import it.Gruppo.NerdMania.Mapper.Converter;
 import it.Gruppo.NerdMania.Mapper.OrdineMapper;
-import it.Gruppo.NerdMania.Modelli.Ordine;
-import it.Gruppo.NerdMania.Modelli.User;
+import it.Gruppo.NerdMania.Modelli.*;
+import it.Gruppo.NerdMania.Repository.CarrelloRepository;
 import it.Gruppo.NerdMania.Repository.OrdineRepository;
 import it.Gruppo.NerdMania.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,16 +18,17 @@ public class OrdineService extends AbstractService<Ordine, OrdineDto>  {
     private final OrdineMapper ordineMapper;
     private final OrdineRepository ordineRepository;
     private final UserRepository userRepository;
+    private final CarrelloRepository carrelloRepository;
 
     @Autowired
     private EmailService emailService;
 
-
-    public OrdineService(JpaRepository<Ordine, Integer> repository, Converter<Ordine, OrdineDto> converter, OrdineMapper ordineMapper, OrdineRepository ordineRepository, UserRepository userRepository) {
+    public OrdineService(JpaRepository<Ordine, Integer> repository, Converter<Ordine, OrdineDto> converter, OrdineMapper ordineMapper, OrdineRepository ordineRepository, UserRepository userRepository, CarrelloRepository carrelloRepository) {
         super(repository, converter);
         this.ordineMapper = ordineMapper;
         this.ordineRepository = ordineRepository;
         this.userRepository= userRepository;
+        this.carrelloRepository=carrelloRepository;
     }
 
     @Override
@@ -41,6 +42,53 @@ public class OrdineService extends AbstractService<Ordine, OrdineDto>  {
                 .orElseThrow(() -> new RuntimeException("User non trovato"));
 
         ordine.setUser(user);
+
+        // 🔹 RECUPERO CARRELLO
+        Carrello carrello = carrelloRepository.findByUser(user)
+                        .orElseThrow(() -> new RuntimeException("User non trovato"));
+
+        // 🔥 COSTO DAL CARRELLO
+        ordine.setCostoTotale(carrello.getPrezzoTotale().floatValue());
+
+        // 🔥 CREAZIONE SPEDIZIONE
+        Spedizione spedizione = new Spedizione();
+
+        // PESO DAL CARRELLO
+        spedizione.setPeso(carrello.getPeso().floatValue());
+
+        // FRAGILE → almeno un prodotto fragile
+        boolean fragile = ordine.getProdotti()
+                .stream()
+                .anyMatch(Prodotto::isFragile);
+
+        spedizione.setFragile(fragile);
+
+        // DIMENSIONI → massimo tra i prodotti
+        float altezzaMax = ordine.getProdotti()
+                .stream()
+                .map(Prodotto::getAltezza)
+                .max(Float::compare)
+                .orElse(0f);
+
+        float lunghezzaMax = ordine.getProdotti()
+                .stream()
+                .map(Prodotto::getLunghezza)
+                .max(Float::compare)
+                .orElse(0f);
+
+        float spessoreMax = ordine.getProdotti()
+                .stream()
+                .map(Prodotto::getSpessore)
+                .max(Float::compare)
+                .orElse(0f);
+
+        spedizione.setAltezza(altezzaMax);
+        spedizione.setLunghezza(lunghezzaMax);
+        spedizione.setSpessore(spessoreMax);
+
+        // 🔗 COLLEGAMENTO BIDIREZIONALE
+        spedizione.setOrdine(ordine);
+        ordine.setSpedizione(spedizione);
 
         Ordine salvato = ordineRepository.save(ordine);
 
